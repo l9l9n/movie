1c610b5904dabc74b262eee69181d20b518fa273
Ulukbek <l9l9ndos@gmail.com>
2023-12-10 20:57:18 +0600

    SetUp project movie

---
+++ movies/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+from rest_framework.urlpatterns import format_suffix_patterns
+
+from movies import views
+
+urlpatterns = [
+
+]
+
+
+
+#     path("movie/", views.MovieViewSet.as_view({'get': 'list'})),
+#     path("movie/<int:pk>/", views.MovieViewSet.as_view({'get': 'retrieve'})),
+#     path("review/", views.ReviewCreateViewSet.as_view({'post': 'create'})),
+#     path("rating/", views.AddStarRatingViewSet.as_view({'post': 'create'})),
+#     path('actor/', views.ActorsViewSet.as_view({'get': 'list'})),
+#     path('actor/<int:pk>/', views.ActorsViewSet.as_view({'get': 'retrieve'})),
